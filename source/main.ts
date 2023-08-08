a2cf78297ff12a65196527821cca77787aa0e8f3
Ruan Carllo Silva <ruan4ks@gmail.com>
2023-08-08 18:17:03 -0300

    🎯 feats: Adaptação do manejo de variáveis de ambiente para o dotenv

--- source/main.ts
+++ source/main.ts
@@ -1,18 +1,18 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+import * as dotenv from 'dotenv';
+
 import DiscordBOT from './libraries/discordbot';
 import ChatGPT from './libraries/chatgpt';
 import Vestractor from './libraries/vestractor';
 import Bibliothecary from './libraries/bibliothecary';
 
 async function main(): Promise<void> {
-  const {OPENAI_TOKEN, DISCORD_TOKEN} = JSON.parse(fs.readFileSync(path.join(__dirname, './tokens.json'), 'utf-8'));
-
-  const discordBOT = new DiscordBOT(DISCORD_TOKEN);
+  const discordBOT = new DiscordBOT(process.env.DISCORD_TOKEN);
   
   discordBOT.setDependencies({
-    chatGPT: new ChatGPT(OPENAI_TOKEN),
+    chatGPT: new ChatGPT(process.env.OPENAI_TOKEN),
     vestractor: await Vestractor.create(),
     bibliothecary: new Bibliothecary(path.join(__dirname, './assets/plurall-bookshelf.yaml'))
   })
@@ -21,4 +21,5 @@ async function main(): Promise<void> {
   if (process.argv.includes('--initialize-client')) await discordBOT.initializeClient();
 }
 
+dotenv.config();
 main();
